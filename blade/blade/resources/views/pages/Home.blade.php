1c467b6b550dc1d158f6b07085bdb086060d1347
nasirdiu <nasir.joypurhat@gmail.com>
2023-05-24 16:02:32 +0600

    blade project

---
+++ blade/blade/resources/views/pages/Home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app');
+
+@section('content')
+    @include('components.Hero')
+    @include('components.HowWeDo')
+    @include('components.NewsLater')
+    @include('components.Priching')
+    @include('components.Tream')
+
+@endsection
