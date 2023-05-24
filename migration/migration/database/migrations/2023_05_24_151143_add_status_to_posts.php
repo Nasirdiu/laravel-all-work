864838c6fa4a8eb48437336bea1cc6711497b4e4
nasirdiu <nasir.joypurhat@gmail.com>
2023-05-24 21:17:16 +0600

    migrate done

--- migration/migration/database/migrations/2023_05_24_151143_add_status_to_posts.php
+++ migration/migration/database/migrations/2023_05_24_151143_add_status_to_posts.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->renameColumn('description','content');
+            $table->integer('status')->default(0);
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->renameColumn('content','description');
+            $table->dropColumn('status');
         });
     }
 };
