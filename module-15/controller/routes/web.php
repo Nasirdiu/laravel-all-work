d16b1ef11e22268dc560102483ea86a562527bd6
nasirdiu <nasir.joypurhat@gmail.com>
2023-05-25 11:31:30 +0600

    controller crud

--- module-15/controller/routes/web.php
+++ module-15/controller/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\DemoController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/hello',[DemoController::class,'DemoAction']);
+Route::get('/single',VideoController::class);
 
+Route::resource('photos',PhotoController::class);
