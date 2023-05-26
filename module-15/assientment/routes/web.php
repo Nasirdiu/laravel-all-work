9139aa930b2e747aff136cb55ed3a985027b1dbd
nasirdiu <nasir.joypurhat@gmail.com>
2023-05-26 10:50:32 +0600

    task2 done

--- module-15/assientment/routes/web.php
+++ module-15/assientment/routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-
+//task8
 Route::get('/', function () {
     return view('welcome');
 });
+
+//task2
+Route::get('/home', function () {
+    return redirect('/dashboard', 302);
+});
+
+
