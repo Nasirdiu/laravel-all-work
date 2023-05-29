273496bac4690af9769c45ed97a4bf3e7564fc86
nasirdiu <nasir.joypurhat@gmail.com>
2023-05-29 11:53:45 +0600

    log

--- module-15/assientment/routes/web.php
+++ module-15/assientment/routes/web.php
@@ -1,15 +1,40 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DemoController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
-//task8
-Route::get('/', function () {
-    return view('welcome');
-});
+// Task 1: Request Validation
+Route::post('/reg', [RegistrationController::class, 'FromRegistration']);
 
-//task2
+//Task 2: Request Redirect
 Route::get('/home', function () {
     return redirect('/dashboard', 302);
 });
 
+// Task 3: Global Middleware
 
+//Task 4: Route Middleware
+Route::middleware(['Demo'])->group(function () {
+    Route::get('/profile', [DemoController::class, 'DemoAction1']);
+    Route::get('/settings', [DemoController::class, 'DemoAction2']);
+});
+
+// Task 5: Controller
+
+Route::resource('product', ProductController::class);
+
+// Task 6: Single Action Controller
+
+Route::get('/contact ', ContactController::class);
+
+// Task 7: Resource Controller
+Route::resource('post', PostController::class);
+
+//Task 8: Blade Template Engine
+Route::get('/', function () {
+    return view('welcome');
+});
