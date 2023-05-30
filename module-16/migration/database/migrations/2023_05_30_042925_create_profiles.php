c2c8510b7561f9c3ae556670deef225ef21af444
nasirdiu <nasir.joypurhat@gmail.com>
2023-05-30 19:34:19 +0600

    tabale

--- module-16/migration/database/migrations/2023_05_30_042925_create_profiles.php
+++ module-16/migration/database/migrations/2023_05_30_042925_create_profiles.php
@@ -12,8 +12,41 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+//            $table->bigIncrements('id');
+//            $table->boolean('is_bangladeshi');
+//            $table->bigInteger('vote');
+//            $table->binary('photo');
+//            $table->char('name','50');
+//            $table->dateTime('voting_date_time');
+//            $table->date('voting_date');
+//            $table->double('population');
+//            $table->enum('group',['A','B']);
+//            $table->id();
+//            $table->float('doller');
+//            $table->geometryCollection('positions');
+//            $table->geometry('positions_2');
+//            $table->integer('population');
+//            $table->ipAddress('visitor');
+//            $table->json('user_details');
+//            $table->longText('blog_post');
+//            $table->smallIncrements('id');
+//            $table->mediumInteger('col1');
+//            $table->mediumText('col2');
+//            $table->smallInteger('col3');
+//            $table->string('col4');
+//            $table->time('col5');
+//            $table->timestamp('col6');
+//            $table->tinyInteger('col8');
+//            $table->unsignedBigInteger('col9');
+//            $table->unsignedInteger('col10');
+//            $table->text('col11');
+//            $table->unsignedMediumInteger('col12');
+//            $table->unsignedTinyInteger('col13');
+//            $table->unsignedSmallInteger('col14');
+//            $table->tinyText('col15');
+//            $table->timestamps();
+
+
         });
     }
 
