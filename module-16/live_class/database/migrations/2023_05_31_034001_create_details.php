47332e678a563ca3756820cc88828bdd4dcb96c3
nasirdiu <nasir.joypurhat@gmail.com>
2023-05-31 13:28:43 +0600

    tabal-product

--- module-16/live_class/database/migrations/2023_05_31_034001_create_details.php
+++ module-16/live_class/database/migrations/2023_05_31_034001_create_details.php
@@ -13,7 +13,23 @@ return new class extends Migration
     {
         Schema::create('details', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('img1',200);
+            $table->string('img2',200);
+            $table->string('img3',200);
+            $table->string('img4',200);
+            $table->longText('des');
+            $table->string('color',200);
+            $table->string('size',200);
+
+            $table->unsignedBigInteger('product_id')->unique();
+
+// relaitionship
+            $table->foreign('product_id')->references('id')->on('products')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
--- module-16/live_class/database/migrations/2023_05_31_034016_create_sliders.php
+++ module-16/live_class/database/migrations/2023_05_31_034016_create_sliders.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            //
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
