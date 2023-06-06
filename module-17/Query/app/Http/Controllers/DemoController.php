b01d3c4a14e00760620a8afe6101a31afccbecb4
nasirdiu <nasir.joypurhat@gmail.com>
2023-06-07 01:53:57 +0600

    advance join

--- module-17/Query/app/Http/Controllers/DemoController.php
+++ module-17/Query/app/Http/Controllers/DemoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -14,17 +15,29 @@ class DemoController extends Controller
 //        $result= DB::table('brands')->pluck('brandImg','brandName');
       //  $result= DB::table('products')->sum('price');//max min avg sum
 //        $result= DB::table('products')->select('title','price')->get();
-        //$result= DB::table('products')->select('title','price')->distinct()->get(); //unique value select korer jono.
+        /*
+       //$result= DB::table('products')->select('title','price')->distinct()->get();
+       // //unique value select korer jono.
+        */
 
-        //inner join,leftjoin,rightjoin,
-      /*  $result= DB::table('products')
-            ->join('categories','products.category_id','=','categories.id')
-            ->join('brands','products.brand_id','=','brands.id')
-            ->get();
-      */
+       //inner join,leftjoin,rightjoin,
+     /*  $result= DB::table('products')
+           ->join('categories','products.category_id','=','categories.id')
+           ->join('brands','products.brand_id','=','brands.id')
+           ->get();
+
+        /*
+       $result= DB::table('products')
+           ->crossJoin('brands')
+           ->get();
+  return $result;
+     */
+
         $result= DB::table('products')
-            ->crossJoin('brands')
-            ->get();
-   return $result;
+            ->join('categories',function (JoinClause $join){
+                $join->on('products.category_id','=','categories.id')
+                    ->where('products.price','=',20);
+            })->get();
+        return $result;
     }
 }
