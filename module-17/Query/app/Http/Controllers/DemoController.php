b95c747192a8d3dbdcc52d26bbc29d5f1347a224
nasirdiu <nasir.joypurhat@gmail.com>
2023-06-07 00:46:20 +0600

    quary_value

--- module-17/Query/app/Http/Controllers/DemoController.php
+++ module-17/Query/app/Http/Controllers/DemoController.php
@@ -11,9 +11,10 @@ class DemoController extends Controller
    //$result= DB::table('brands')->get();
 
         //first & find method
-
-        $result= DB::table('brands')->pluck('brandImg','brandName');
-
+//        $result= DB::table('brands')->pluck('brandImg','brandName');
+      //  $result= DB::table('products')->sum('price');//max min avg sum
+//        $result= DB::table('products')->select('title','price')->get();
+        $result= DB::table('products')->select('title','price')->distinct()->get(); //unique value select korer jono.
    return $result;
     }
 }
