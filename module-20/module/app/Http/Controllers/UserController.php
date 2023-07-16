2c110c911f2d695ce1a305b8ae3d885dee8ec1b4
nasirdiu <nasir.joypurhat@gmail.com>
2023-07-16 10:57:56 +0600

    module-create reg

--- module-20/module/app/Http/Controllers/UserController.php
+++ module-20/module/app/Http/Controllers/UserController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    function UserRegistration(Request $request){
+        try {User::create([
+            'firstName'=>$request->input('firstName'),
+            'lastName'=>$request->input('lastName'),
+            'email'=>$request->input('email'),
+            'mobile'=>$request->input('mobile'),
+            'password'=>$request->input('password'),
+
+        ]);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Registration Successfully'
+            ],200);
+        }catch (\Exception $e){
+            return response()->json([
+                'status' => 'Fail',
+                'message' => 'User Registration Fail'
+//                'message' => $e->getMessage()
+            ],200);
+        }
+
+    }
+
+
 }
