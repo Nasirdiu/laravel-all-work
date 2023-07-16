98a56a0f8eb65c1d297ad8df04fb155424476b81
nasirdiu <nasir.joypurhat@gmail.com>
2023-07-16 12:31:20 +0600

    module_otp send sucess

--- module-20/module/app/Http/Controllers/UserController.php
+++ module-20/module/app/Http/Controllers/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Helper\JWTToken;
+use App\Mail\OTPMail;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
@@ -50,6 +52,28 @@ class UserController extends Controller
             ],200);
         }
 
+    }
+
+    function SendOTPCode(Request $request){
+        $email=$request->input('email');
+        $otp=rand(10000,99999);
+        $count=User::where('email','=',$email)->count();
+        if($count==1){
+            // OTP Email Address
+            Mail::to($email)->send(new OTPMail($otp));
+            // OTO Code Table Update
+            User::where('email','=',$email)->update(['otp'=>$otp]);
+            return response()->json([
+                'status' => 'success',
+                'message' => '4 Digit OTP Code has been send to your email !'
+            ],200);
+
+        }else{
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'unauthorized'
+            ]);
+        }
     }
 
 
--- module-20/module/routes/web.php
+++ module-20/module/routes/web.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\UserController;
 
 Route::post('/user-registration',[UserController::class,'UserRegistration']);
 Route::post('/user-login',[UserController::class,'UserLogin']);
+Route::post('/SendOTPCode',[UserController::class,'SendOTPCode']);
+
