1bad5121357efda91c08dee80c3caf7e2053def3
nasirdiu <nasir.joypurhat@gmail.com>
2023-07-18 18:38:09 +0600

    blade done

--- module-20/module/routes/web.php
+++ module-20/module/routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Middleware\TokenVerificationMiddleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Middleware\TokenVerificationMiddleware;
 
 
 Route::post('/user-registration',[UserController::class,'UserRegistration']);
@@ -13,4 +14,12 @@ Route::post('/VerifyOTP',[UserController::class,'VerifyOTP']);
 //token verify
 Route::post('/ResetPassword',[UserController::class,'ResetPassword'])
 ->middleware([TokenVerificationMiddleware::class]);
+
+// Page Routes
+Route::get('/userLogin',[UserController::class,'LoginPage']);
+Route::get('/userRegistration',[UserController::class,'RegistrationPage']);
+Route::get('/sendOtp',[UserController::class,'SendOtpPage']);
+Route::get('/verifyOtp',[UserController::class,'VerifyOTPPage']);
+Route::get('/resetPassword',[UserController::class,'ResetPasswordPage']);
+Route::get('/dashboard',[DashboardController::class,'DashboardPage']);
 
