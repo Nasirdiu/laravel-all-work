130248c421601a15b9b2c5604f06cf8714170041
nasirdiu <nasir.joypurhat@gmail.com>
2023-07-18 19:30:19 +0600

    module copy

--- module-20/module - Copy/routes/web.php
+++ module-20/module - Copy/routes/web.php
@@ -15,11 +15,4 @@ Route::post('/verify-otp',[UserController::class,'VerifyOTP']);
 Route::post('/reset-password',[UserController::class,'ResetPassword'])
 ->middleware([TokenVerificationMiddleware::class]);
 
-// Page Routes
-Route::get('/userLogin',[UserController::class,'LoginPage']);
-Route::get('/userRegistration',[UserController::class,'RegistrationPage']);
-Route::get('/sendOtp',[UserController::class,'SendOtpPage']);
-Route::get('/verifyOtp',[UserController::class,'VerifyOTPPage']);
-Route::get('/resetPassword',[UserController::class,'ResetPasswordPage']);
-Route::get('/dashboard',[DashboardController::class,'DashboardPage']);
 
