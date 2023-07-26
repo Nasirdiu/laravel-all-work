4f587ed2ea533bb344c578b02f69599d657a2267
nasirdiu <nasir.joypurhat@gmail.com>
2023-07-26 10:59:11 +0600

    assientment

--- module-21/assientment/pos_project/routes/web.php
+++ module-21/assientment/pos_project/routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::post('/UserRegistration',[AuthenticationController::class,'UserRegistration']);
-Route::post('/UserLogin',[AuthenticationController::class,'UserLogin']);
-
-
+Route::get( '/login', function () {
+    return view( 'welcome' );
+} );
